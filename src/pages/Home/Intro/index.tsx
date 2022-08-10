0ad401d9d9b670403773d73925bb22c9b9365306
Diogo Novaes <diogonovaes@gmail.com>
2022-08-10 22:11:25 +0200

    started intro hero

--- src/pages/Home/Intro/index.tsx
+++
@@ -1,5 +0,0 @@
-export function Intro() {
-  return (
-    <h1>Intro Hero</h1>
-  );
-}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,10 @@
+import { Intro } from "./components/Intro";
+import { HomeContainer } from "./styles";
+
 export function HomePage() {
-  return <h1>HOME</h1>;
+  return (
+    <HomeContainer>
+      <Intro />
+    </HomeContainer>
+  );
 }
