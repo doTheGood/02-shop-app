f41186974032fac449c32ce0cebd9e69e51ab097
Diogo Novaes <diogonovaes@gmail.com>
2022-08-20 22:06:41 +0200

    interface visual finished

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,8 +1,14 @@
 import { RegularText, TitleText } from "../../components/Typography";
 import { OrderConfirmedContainer, OrderDetailsContainer } from "./styles";
 import confirmedOrderIllustration from "../../assets/confirmed-order.svg";
+import { useTheme } from "styled-components";
+import { InfoWithIcon } from "../../components/InfoWithIcon";
+import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
 
 export function OrderConfirmed() {
+
+  const { colors } = useTheme();
+
   return (
     <OrderConfirmedContainer className="container">
       <div>
@@ -13,7 +19,39 @@ export function OrderConfirmed() {
       </div>
       <section>
         <OrderDetailsContainer>
-          <p>oi</p>
+          <InfoWithIcon
+            icon={<MapPin weight="fill" />}
+            iconBg={colors["brand-purple"]}
+            text={
+              <RegularText>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                <br />
+                Farrapos - Porto Alegre, RS
+              </RegularText>
+            }
+          />
+          <InfoWithIcon
+            icon={<Clock weight="fill" />}
+            iconBg={colors["brand-yellow"]}
+            text={
+              <RegularText>
+                Previsão de Entrega
+                <br />
+                <strong>20 min - 30 min</strong>
+              </RegularText>
+            }
+          />
+          <InfoWithIcon
+            icon={<CurrencyDollar weight="fill" />}
+            iconBg={colors["brand-yellow-dark"]}
+            text={
+              <RegularText>
+                Pagamento na Entraga
+                <br />
+                <strong>Cartão de crédito</strong>
+              </RegularText>
+            }
+          />
         </OrderDetailsContainer>
         <img src={confirmedOrderIllustration} />
       </section>
